ce2c2043fce951a8ec6e4407db9c861a2116b7e5
Bryan <bhtruong93@gmail.com>
2015-11-17 19:44:03 -0800

    getting text editors with codemirror

---
+++ client/main.js
@@ -0,0 +1,21 @@
+var myCodeMirror = CodeMirror(document.body, {
+  value: "function myScript(){return 100;}\n",
+  mode:  "javascript"
+});
+
+// var myCodeMirror2 = CodeMirror(document.body, {
+//   value: "function myScript(){return 100;}\n",
+//   mode:  "javascript"
+// });
+
+myCodeMirror.linkedDoc();
+
+var myCodeMirror3 = CodeMirror(document.body, {
+  value: "function myScript(){return 100;}\n",
+  mode:  "javascript"
+});
+
+var myCodeMirror4 = CodeMirror(document.body, {
+  value: "function myScript(){return 100;}\n",
+  mode:  "javascript"
+});
