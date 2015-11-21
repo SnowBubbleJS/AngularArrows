807caa3909a13d99e5658926f59ab3f6316d2897
Bryan <bhtruong93@gmail.com>
2015-11-20 16:32:31 -0800

    updates

--- client/main.js
+++ client/main.js
@@ -1,7 +1,15 @@
 var myCodeMirror = CodeMirror(document.body, {
+  lineNumbers: true,
   value: "function myScript(){return 100;}\n",
   mode:  "javascript"
 });
+
+function submit() {
+  var temp = myCodeMirror.getValue();
+  var func = new Function(temp);
+  func();
+
+}
 
 // var myCodeMirror2 = CodeMirror(document.body, {
 //   value: "function myScript(){return 100;}\n",
@@ -11,11 +19,15 @@ var myCodeMirror = CodeMirror(document.body, {
 myCodeMirror.linkedDoc();
 
 var myCodeMirror3 = CodeMirror(document.body, {
+  lineNumbers: true,
   value: "function myScript(){return 100;}\n",
   mode:  "javascript"
 });
 
 var myCodeMirror4 = CodeMirror(document.body, {
+  lineNumbers: true,
   value: "function myScript(){return 100;}\n",
   mode:  "javascript"
 });
+
+// document.querySelector(".CodeMirror").addLineClass(1, 'background', "highlighted-line");
